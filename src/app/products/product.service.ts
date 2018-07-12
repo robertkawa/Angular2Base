8de22e9846809c7ce75f4a092b376f75ce33465b
Robert Kawa <robertkawa1992@gmail.com>
2018-07-12 18:19:36 +0200

    inject product service

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from "@angular/core";
 import { IProduct } from "./product";
 
-@Injectable()
+@Injectable({
+    providedIn: 'root'
+})
+
 export class ProductService {
     getProducts(): IProduct[]{
         return [
